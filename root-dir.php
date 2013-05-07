d5fed5331017b132fd8780b554161dbdca6eb265
ghyghoo8 <ghyghoo8@qq.com>
2013-05-07 20:09:20 +0800

    根目录combo策略

---
+++ root-dir.php
@@ -0,0 +1,29 @@
+/**
+根目录下的combo脚本
+**/
+<?php
+
+$LOCAL_ROOT = dirname($_SERVER['SCRIPT_FILENAME']);
+$REAL_CDN = "http://assets.taobaocdn.com";
+
+$parts = explode("??", $_SERVER["REQUEST_URI"]);
+$base = $parts[0];
+$files = explode(",", $parts[1]);
+
+if (strpos($files[0], ".js")) {
+    header("Content-type: application/x-javascript");
+} else {
+    header("Content-type: text/css");
+}
+
+foreach($files as $file) {
+    $url = $base.$file;
+    $root = $REAL_CDN;
+
+    if(file_exists($LOCAL_ROOT.$url)) {
+        echo "/* fetched from local file system */\n";
+        $root = $LOCAL_ROOT;
+    }
+
+    echo file_get_contents($root.$url);
+}
